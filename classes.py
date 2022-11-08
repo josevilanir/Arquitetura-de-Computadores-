b787d5b5aff5591d90c9f3a85c48f2d895f6e7c1
josevilanir <104863215+josevilanir@users.noreply.github.com>
2022-11-08 11:57:46 -0300

    Funções Montador
    Biblioteca e funções que o programa se baseia para reconhecer as instruções do arquivo de texto.

--- classes.py
+++ classes.py
@@ -2,7 +2,6 @@ from turtle import rt
 import math
 registradores = {'$0 ':"0",'$at':'1','$v0':'2','$v1':'3','$a0':'4','$a1':'5','$a2':'6','$a3':'7','$t0':'8','$t1':'9','$t2':'10','$t3':'11','$t4':'12','$t5':'13','$t6':'14','$t7':'15','$s0':'16','$s1':'17','$s2':'18','$s3':'19','$s4':'20','$s5':'21','$s6':'22','$s7':'23','$t8':'24','$t9':'25','$k0':'26','$k1':'27','$gp':'28','$sp':'29','$fp':'30','$ra':'31'}
 
-
 def separador_rs(linha_codigo):
   rs = linha_codigo[8]+linha_codigo[9]+linha_codigo[10]
   rs_regis = registradores[rs]
@@ -29,7 +28,10 @@ def separadordiv_rt(linha_codigo):
   rt_regis = registradores[rt]
   return rt_regis
 
-
+def immedieate(linha_codigo):
+    constante = linha_codigo[12]+linha_codigo[13]
+    constante=  str(constante)
+    return constante
 
 
 def classe(linha_codigo):
